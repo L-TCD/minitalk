11bdeb0fbf9f230773f917c3e9ca53e8d27e05f9
L-TCD <loic.coissard@gmail.com>
2022-12-15 10:42:04 +0100

    server + client working but not finished

--- src/client.c
+++ src/client.c
@@ -10,11 +10,76 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <limits.h>//
+#include <signal.h>
+#include <unistd.h>
 
-int main(int ac, char **av)
+static void	ft_putstr(char *str)
 {
-	printf("ac: %d\n", ac);
-	(void)av;
+	while (*str)
+		write(1, &*str++, 1);
+}
+
+// à corriger
+int	ft_atoi(const char *str)
+{
+	int						i;
+	int						sign;
+	long long unsigned int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			sign = -sign;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i++] - '0');
+		if (result > LLONG_MAX)
+		{
+			if (sign > 0)
+				return (-1);
+			else
+				return (0);
+		}
+	}
+	return (sign * result);
+}
+
+void	sig_sender(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (1 << bit)) != 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		bit++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (ac != 3)
+	{
+		ft_putstr("error\n");
+		return (1);
+	}
+	pid = ft_atoi(av[1]);
+	//secure pid ?
+	while (av[2][i])
+		sig_sender(pid, av[2][i++]);
+	sig_sender(pid, '\n');
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -10,10 +10,70 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+static void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, &*str++, 1);
+}
+
+static void	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n == -2147483648)
+		ft_putstr("-2147483648");
+	else
+	{
+		if (n < 0)
+		{
+			n = -n;
+			write(1, &"-", 1);
+		}
+		if (n > 9)
+		{
+			ft_putnbr((n / 10));
+			c = n % 10 + '0';
+			write(1, &c, 1);
+		}
+		else
+		{
+			c = n + '0';
+			write(1, &c, 1);
+		}
+	}
+}
+
+static void	sig_handler(int signal)
+{
+	static int	bit = 0;
+	static int	i = 0;
+
+	if (signal == SIGUSR1)
+		i |= (1 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		write(1, &i, 1);
+		bit = 0;
+		i = 0;
+	}
+
+}
 
 int main(void)
 {
-	printf("server On\n");
+	int	pid;
+
+	pid = getpid();
+	ft_putstr("pid :");
+	ft_putnbr(pid);
+	ft_putstr("\n");
+	signal(SIGUSR1, &sig_handler);
+	signal(SIGUSR2, &sig_handler);
+	while (1)
+		pause();
 	return (0);
 }
